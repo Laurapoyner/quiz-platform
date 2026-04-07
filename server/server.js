b32666a2a4991bd51e30f24c1d3f306c1d99c327
Tobias <tobias5704@gmail.com>
2026-04-07 12:59:06 +0200

    Server opsætning inkl. routes.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,22 @@
 //server
+const express = require("express");
+const app = express();
+
+//import af routes
+const adminRoutes = require("./routes/admin");
+const authRoutes = require("./routes/auth");
+const quizRoutes = require("./routes/quiz");
+
+app.use(express.json());
+
+//routes
+app.use("/api", adminRoutes);
+app.use("/api", authRoutes);
+app.use("/api", quizRoutes);
+
+app.listen(3000, () => {
+    console.log("Serverport: 3000");
+})
 
 // funktioner der laves her
 //her1
