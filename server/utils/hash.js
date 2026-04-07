647b8132c08a666a0fcc4aa34a71a02162fa74cb
Tobias <tobias5704@gmail.com>
2026-04-07 15:11:28 +0200

    Bcrypt setup, hashing & salting

--- server/utils/hash.js
+++ server/utils/hash.js
@@ -1,5 +1,21 @@
 //hash
 
-//shuffle spørgsmål
+//shuffle spï¿½rgsmï¿½l
 //sanitize HTML
 //hash password
+
+//bcrypt lib
+const bcrypt = require("bcrypt");
+
+//Hashing function & salting 10 rounds
+const hashPassword = async (password) => {
+    return await bcrypt.hash(password, 10);
+};
+
+
+
+const comparePassword = async (password, hash) => {
+    return await bcrypt.compare(password, hash);
+};
+
+module.exports = { hashPassword, comparePassword };
