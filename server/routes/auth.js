2820431cb82f2fe8dc97852b2ce0ba5c9f0556c7
Tobias <tobias5704@gmail.com>
2026-04-08 12:48:58 +0200

    middleware og auth setup,samt user write and read

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,3 +3,12 @@
 // funktioner der laves her
 //her1
 //her2
+
+const express = require("express");
+const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+
+const {hashPassword, comparePassword} = require("../utils/hash");
+const {readUsers, writeUsers} = require("../utils/file")
+const {auth} = require("../middleware/authMiddleware");
