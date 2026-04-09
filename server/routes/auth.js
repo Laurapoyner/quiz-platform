c38ab8cd7372d5cba490a892e75f6d4f4970295a
Tobias <tobias5704@gmail.com>
2026-04-09 08:37:43 +0200

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,4 @@
 //auth
-/**
- * Midlertidig router
- * Skal udvides med rigtige endpoints
- */
-const express = require('express');
-const router = express.Router();
-
-router.get('/auth/test', (req, res) => {
-    res.json({ message: "auth route works" });
-});
-
-module.exports = router;
 // funktioner der laves her
 //her1
 //her2
