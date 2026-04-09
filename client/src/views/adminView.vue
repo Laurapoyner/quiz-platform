d464098c0b42b9df5fa92c6e89316ced61c46ac3
Annemette M <annemettemygind@gmail.com>
2026-04-09 14:54:56 +0200

    Brugerresultater table styling

--- client/src/views/adminView.vue
+++ client/src/views/adminView.vue
@@ -35,45 +35,55 @@
       </div>
 
       <!--- Quiz liste --->
-      <div class="quiz-list">
+      <div>
         <h3>Uploadede quizzer</h3>
-
-        <ul v-if="quizzes.length">
-          <li class="quiz-item" v-for="quiz in quizzes" :key="quiz">
-            {{ quiz.replace(".xml", "") }}
-            <button class="quiz-item-btn" @click="testQuiz(quiz)">Test</button>
-            <button class="quiz-item-btn" @click="deleteQuiz(quiz)">
-              Slet
-            </button>
-          </li>
-        </ul>
-        <p v-else>Ingen quizzer fundet</p>
+        <div class="quiz-list">
+          <ul v-if="quizzes.length">
+            <li class="quiz-item" v-for="quiz in quizzes" :key="quiz">
+              {{ quiz.replace(".xml", "") }}
+              <div class="admin-quiz-btns">
+                <button class="quiz-item-btn" @click="testQuiz(quiz)">
+                  Test Quiz
+                </button>
+                <button class="quiz-item-btn" @click="deleteQuiz(quiz)">
+                  Slet
+                </button>
+              </div>
+            </li>
+          </ul>
+          <p v-else>Ingen quizzer fundet</p>
+        </div>
       </div>
     </div>
 
     <!-- Brugerresultater  -->
-    <div class="column">
+
+    <div class="results-section">
       <h3>Brugerresultater</h3>
-      <table v-if="results.length" class="results-table">
-        <thead>
-          <tr>
-            <th>Bruger</th>
-            <th>Quiz</th>
-            <th>Score</th>
-            <th>Tid (sek)</th>
-            <th>Dato</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="r in results" :key="r.attemptId[0]">
-            <td>{{ r.userId[0] }}</td>
-            <td>{{ r.quizId[0] }}</td>
-            <td>{{ r.score[0] }}</td>
-            <td>{{ r.time[0] }}</td>
-            <td>{{ formatDate(r.date[0]) }}</td>
-          </tr>
-        </tbody>
-      </table>
+
+      <div class="results-table-container" v-if="results.length">
+        <table class="results-table">
+          <thead>
+            <tr>
+              <th>Bruger</th>
+              <th>Quiz</th>
+              <th>Score</th>
+              <th>Tid (sek)</th>
+              <th>Dato</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="r in results" :key="r.attemptId[0]">
+              <td class="userid">{{ r.userId[0] }}</td>
+              <td>{{ r.quizId[0] }}</td>
+              <td class="score">{{ r.score[0] }}</td>
+              <td>{{ r.time[0] }}</td>
+              <td>{{ formatDate(r.date[0]) }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
       <p v-else>Ingen resultater endnu</p>
     </div>
   </div>
@@ -128,7 +138,7 @@ export default {
         if (res.ok) {
           alert("Quiz uploadet!");
           this.selectedFile = null;
-          await this.loadQuizzes(); // opdater listen automatisk
+          await this.loadQuizzes();
         } else {
           const err = await res.text();
           alert("Upload fejl: " + err);
